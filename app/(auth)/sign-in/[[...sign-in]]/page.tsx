3abb8e8b8cf94eec19b4f3f44630f1a22ec3eb73
Anoop Singh <anooppanesar@Anoops-MBP.lan>
2024-08-19 14:45:35 -0600

    initial commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn, SignUp } from '@clerk/nextjs';
+
+const SignInPage = () => {
+  return (
+    <SignIn />
+  );
+}
+
+export default SignInPage;
---
+++ lib/database/mongoose.ts
@@ -0,0 +1,28 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL!).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
